ec0665363ca6a001c53e8901d4794359cac0be21
nicolmendoza <nicolmendozamattos@gmail.com>
2022-06-19 16:11:26 -0500

    tercer commit3

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'molinera4',
-    database: 'notesapp'
+    host: 'bsozqtcwp71w75kshcvf-mysql.services.clever-cloud.com',
+    user: 'untifwtys306x7qz',
+    password: 'AnJ0aX6lmoxtS9HvLzO8',
+    database: 'bsozqtcwp71w75kshcvf'
 })
 
 
